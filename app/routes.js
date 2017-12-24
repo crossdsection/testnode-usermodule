a51060800d7ffd01924d7336415665d22d20d9af
crossdsection <chirkut.kr@gmail.com>
2017-12-24 16:23:55 +0530

    Removed EJS added consolidate to render html with mustache

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@ module.exports = function(app, passport) {
     // HOME PAGE (with login links) ========
     // =====================================
     app.get('/', function(req, res) {
-        res.render('index.ejs'); // load the index.ejs file
+        res.render('index.html'); // load the index.html file
     });
 
     // =====================================
@@ -15,7 +15,7 @@ module.exports = function(app, passport) {
     app.get('/login', function(req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.render('login.ejs', { message: req.flash('loginMessage') }); 
+        res.render('login.html', { message: req.flash('loginMessage') });
     });
 
     // process the login form
@@ -28,7 +28,7 @@ module.exports = function(app, passport) {
     app.get('/signup', function(req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.render('signup.ejs', { message: req.flash('signupMessage') });
+        res.render('signup.html', { message: req.flash('signupMessage') });
     });
 
     // process the signup form
@@ -40,7 +40,7 @@ module.exports = function(app, passport) {
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('profile.ejs', {
+        res.render('profile.html', {
             user : req.user // get the user out of session and pass to template
         });
     });
@@ -58,7 +58,7 @@ module.exports = function(app, passport) {
             failureRedirect : '/'
         })
 	);
-     
+
     // =====================================
     // GOOGLE ROUTES =======================
     // =====================================
@@ -74,7 +74,7 @@ module.exports = function(app, passport) {
                     failureRedirect : '/'
             })
 	);
-	
+
     // =====================================
     // LOGOUT ==============================
     // =====================================
@@ -82,24 +82,24 @@ module.exports = function(app, passport) {
         req.logout();
         res.redirect('/');
     });
-    
+
     // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect : '/profile', // redirect to the secure profile section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
-	
+
 	// process the login form
 	app.post('/login', passport.authenticate('local-login', {
         successRedirect : '/profile', // redirect to the secure profile section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
-    
+
 	// locally --------------------------------
 	app.get('/connect/local', function(req, res) {
-		res.render('connect-local.ejs', { message: req.flash('loginMessage') });
+		res.render('connect-local.html', { message: req.flash('loginMessage') });
 	});
 	app.post('/connect/local', passport.authenticate('local-signup', {
 		successRedirect : '/profile', // redirect to the secure profile section
@@ -142,13 +142,13 @@ module.exports = function(app, passport) {
 			failureRedirect : '/'
 		})
 	);
-	
+
 };
 
 // route middleware to make sure a user is logged in
 function isLoggedIn( req, res, next ) {
 
-    // if user is authenticated in the session, carry on 
+    // if user is authenticated in the session, carry on
     if ( req.isAuthenticated() )
         return next();
 
--- server.js
+++ server.js
@@ -11,8 +11,10 @@ var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
+var engines = require('consolidate');
 
 var configDB = require('./config/database.js');
+
 
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
@@ -24,7 +26,9 @@ app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
 
-app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('views', __dirname + '/views');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
 
 // required for passport
 app.use(session({ secret: 'mysessionsecret007' })); // session secret
